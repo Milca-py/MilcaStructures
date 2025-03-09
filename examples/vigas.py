c996a42e8e6813cbbe5c6bd946efe742a06095e0
AMILCAR <166049998+Milca-py@users.noreply.github.com>
2025-03-09 14:16:11 -0500

    Mensaje descriptivo del cambio

--- examples/vigas.py
+++ examples/vigas.py
@@ -10,7 +10,7 @@ def run():
 
     # parameters:
     l = 5
-    n_tramos = 3
+    n_tramos = 4
     q = 50
     for i in range(1, n_tramos + 2):
         sys.add_node(i, (i*l, 0))
@@ -23,20 +23,20 @@ def run():
         sys.add_distributed_load(i, "CARGA", "GLOBAL", q, q, direction="GRAVITY")
 
 
-    sys.solve()
+    # sys.solve()
     sys.node_map[2].restraints = (False, False, False)
-    sys.global_stiffness_matrix[4, 4] += 10000000
-    sys.global_stiffness_matrix[5, 5] += 10000000
-    sys.global_stiffness_matrix[6, 6] += 10000000
+    # sys.global_stiffness_matrix[4, 4] += 10000000
+    # sys.global_stiffness_matrix[5, 5] += 10000000
+    # sys.global_stiffness_matrix[6, 6] += 10000000
     sys.add_point_load(2, "CARGA", "GLOBAL", fy=-100)
     sys.solve()
     sys.plotter.plot_structure(labels_distributed_loads=True, show=False, labels_point_loads=True)
     # sys.plotter.show_diagrams("axial_force", show=False)
-    sys.plotter.show_diagrams("shear_force", show=False, escala=0.003)
-    # sys.plotter.show_diagrams("bending_moment", show=False, escala=0.001)
+    # sys.plotter.show_diagrams("shear_force", show=False, escala=0.003)
+    sys.plotter.show_diagrams("bending_moment", show=False, escala=0.001)
     # sys.plotter.show_diagrams("slope", show=False, escala=4000)
     # sys.plotter.show_diagrams("deflection", show=False, escala=4000, fill=False)
-    # sys.plotter.show_deformed(10, show=False)
+    sys.plotter.show_deformed(10, show=False)
     # sys.plotter.show_rigid_deformed(1000, show=False)
 
     root = create_plot_window(sys.plotter.fig)
