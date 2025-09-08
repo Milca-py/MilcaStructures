a5ae07e63262ca739dd68ddfd01e7fb17cf11e52
Amilcar_Machacca <166049998+Milca-py@users.noreply.github.com>
2025-09-08 18:47:37 -0500

    Delete milcapy directory

--- milcapy/loads/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Módulo de cargas estructurales.
-
-Proporciona clases y funciones para definir y manipular cargas en un modelo estructural, 
-incluyendo cargas puntuales y patrones de carga.
-"""
-
-from ..loads.load import PointLoad, DistributedLoad
-from ..loads.load_pattern import LoadPattern
